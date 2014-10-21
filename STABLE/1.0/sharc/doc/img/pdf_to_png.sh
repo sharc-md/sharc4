c8c6d1041eb74d7f352830130f77c546aecd0a4a
Sebastian Mai <sebastian.mai@univie.ac.at>
2014-10-21 11:48:29 +0200

    resized pngs

--- STABLE/1.0/sharc/doc/img/pdf_to_png.sh
+++ STABLE/1.0/sharc/doc/img/pdf_to_png.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-for i in *.pdf;
+# for i in *.pdf;
+# do
+# 
+#   name=$(echo $i | sed 's/.pdf//')
+#   convert -density 600 $name.pdf $name.png
+# 
+# done
+
+for i in *.png;
 do
 
-  name=$(echo $i | sed 's/.pdf//')
-  convert -density 600 $name.pdf $name.png
+  convert -resize 25% $i temp.png
+  mv temp.png $i
 
 done
