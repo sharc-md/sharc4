e7273f4589399477a14e821edf8ce8a77732b6c6
Sebastian Mai <sebastian.mai@univie.ac.at>
2017-11-29 12:09:08 +0100

    QMout_print.py: option Diagonalize added

--- DEVELOPMENT/bin/QMout_print.py
+++ DEVELOPMENT/bin/QMout_print.py
@@ -11,6 +11,13 @@ import os
 import pprint
 from optparse import OptionParser
 
+try:
+  import numpy
+  NONUMPY=False
+except ImportError:
+  import subprocess as sp
+  NONUMPY=True
+  
 # =========================================================0
 # compatibility stuff
 
@@ -145,6 +152,91 @@ def read_QMin(path,request):
   #pprint.pprint(QMin)
   return QMin
 
+# ======================================================================================================================
+
+class diagonalizer:
+  def __init__(self):
+    exe=os.getenv('SHARC')
+    exe=os.path.expanduser(os.path.expandvars(exe))+'/diagonalizer.x'
+    if not os.path.isfile(exe):
+      print 'SHARC auxilliary diagonalizer not found at %s!' % (exe)
+      sys.exit(1)
+    self.exe=exe
+  def eigh(self,H):
+    STDIN='C %i %i\nTitle\n' % (len(H),len(H))
+    for x in H:
+      for y in x:
+        STDIN+='%20.13f %20.13f ' % (y.real,y.imag)
+      STDIN+='\n'
+    proc=sp.Popen(self.exe,stdin=sp.PIPE,stdout=sp.PIPE)
+    STDOUT=proc.communicate(input=STDIN)[0].split('\n')
+    shift=1
+    for ix in range(len(H)):
+      line=STDOUT[shift+ix].split()
+      for iy in range(len(H)):
+        H[ix][iy]=complex(float(line[0+2*iy]),float(line[1+2*iy]))
+    U=[ [ 0. for i in range(len(H)) ] for j in range(len(H)) ]
+    shift=2+len(H)
+    for ix in range(len(H)):
+      line=STDOUT[shift+ix].split()
+      for iy in range(len(H)):
+        U[ix][iy]=complex(float(line[0+2*iy]),float(line[1+2*iy]))
+    return H,U
+
+# ======================================================================================================================
+
+def transform(H,DM,P):
+  '''transforms the H and DM matrices in the representation where H is diagonal.'''
+
+  if NONUMPY:
+    diagon=diagonalizer()
+    H,U=diagon.eigh(H)
+    UDMU=[ [ [ 0. for i in range(len(H)) ] for j in range(len(H)) ] for k in range(3) ]
+    for xyz in range(3):
+      temp=[ [ 0. for i in range(len(H)) ] for j in range(len(H)) ]
+      for a in range(len(H)):
+        for b in range(len(H)):
+          for i in range(len(H)):
+            temp[a][b]+=U[i][a].conjugate()*DM[xyz][i][b]
+      for a in range(len(H)):
+        for b in range(len(H)):
+          for i in range(len(H)):
+            UDMU[xyz][a][b]+=temp[a][i]*U[i][b]
+    DM=UDMU
+
+    if P!=None:
+      UPU=[ [ 0. for i in range(len(H)) ] for j in range(len(H)) ]
+      for a in range(len(H)):
+        for b in range(len(H)):
+          for i in range(len(H)):
+            UPU[a][b]+=U[i][a].conjugate()*P[i][b]
+      P=[ [ 0. for i in range(len(H)) ] for j in range(len(H)) ]
+      for a in range(len(H)):
+        for b in range(len(H)):
+          for i in range(len(H)):
+            P[a][b]+=temp[a][i]*U[i][b]
+
+  else:
+    eig,U=numpy.linalg.eigh(H)
+    Ucon=[ [ 0. for i in range(len(H)) ] for j in range(len(H)) ]
+    for ix in range(len(U)):
+      for iy in range(len(U)):
+        Ucon[ix][iy]=U[iy][ix].conjugate()
+        if ix==iy:
+          H[ix][iy]=complex(eig[ix])
+        else:
+          H[ix][iy]=complex(0)
+    UDMU=[0,0,0]
+    for xyz in range(3):
+      UDMU[xyz]=numpy.dot(Ucon,numpy.dot(DM[xyz],U))
+    DM=UDMU
+
+    if P!=None:
+      UPU=numpy.dot(Ucon,numpy.dot(P,U))
+      P=UPU
+
+  return H,DM,U
+
 # ========================== Main Code =============================== #
 def main():
 
@@ -162,6 +254,7 @@ excitation energies and oscillator strengths.
   parser.add_option('-e', dest='e', type=float, nargs=1, default=0.0, help="Absolute energy shift (float, default=0.0)")
   parser.add_option('-s', dest='s', type=str, nargs=1, default='', help="Number of states (in quotes separated by whitespace)")
   parser.add_option('-n', dest='n', type=int, nargs=1, default=1, help="Number of atoms")
+  parser.add_option('-D', dest='D', action='store_true',help="Diagonalize")
 
   #parser.add_option('-n', dest='n', type=int, nargs=1, default=3, help="Number of geometries to be generated (integer, default=3)")
   #parser.add_option('-r', dest='r', type=int, nargs=1, default=16661, help="Seed for the random number generator (integer, default=16661)")
@@ -201,10 +294,13 @@ excitation energies and oscillator strengths.
 
   QMout=read_QMout(qmoutfile,QMin['nmstates'],QMin['natom'],['h','dm'])
 
-  #print 'Number of states: %s\n' % (states)
-  #print 'State   E_tot (Eh)\t\tDMX\t\tDMY\t\tDMZ\t\tdE (eV)\t\tf_osc\n'
-  #print '%5s %11s  %16s %12s %12s' % ('State','Label','E (E_h)','dE (eV)','f_osc')
+  sys.stderr.write( 'Number of states: %s\n' % (states) )
+  sys.stderr.write( '%5s  %11s %16s %12s %12s   %6s\n' % ('State','Label','E (E_h)','dE (eV)','f_osc','Spin') )
 
+  if options.D:
+    h,dm,U=transform(QMout['h'][0],QMout['dm'],None)
+    QMout['h']=[h]
+    QMout['dm']=dm
 
   #pprint.pprint(QMin)
   #pprint.pprint(QMout)
@@ -212,27 +308,58 @@ excitation energies and oscillator strengths.
 
   energies=[]
   fosc=[]
-  for istate in range(QMin['nmstates']):
-    m,s,ms=QMin['statemap'][istate+1]
-    if 2*ms+1!=m:
-      continue
-    e=QMout['h'][0][istate][istate].real
-    energies.append(e)
-    if m==1 and s>0:
+  if options.D:
+    for istate in range(QMin['nmstates']):
+      e=QMout['h'][0][istate][istate].real
+      energies.append(e)
+      # spin
+      spin=0.
+      ist=0
+      imax=0.
+      for jstate in range(QMin['nmstates']):
+        m,s,ms=QMin['statemap'][jstate+1]
+        c=(U[jstate][istate]*U[jstate][istate].conjugate()).real
+        spin+=m*c
+        if c>imax:
+          ist=(m,s)
+          imax=c
+      # fosc
       dmx=QMout['dm'][0][istate][0].real
       dmy=QMout['dm'][1][istate][0].real
       dmz=QMout['dm'][2][istate][0].real
       f=2./3.*(e-energies[0])*(dmx**2+dmy**2+dmz**2)
       fosc.append(f)
-    else:
-      dmx=dmy=dmz=0.
-      fosc.append(0.)
-    if ezero!=0.0:
-      de=(e-ezero)*27.21
-    else:
-      de=(e-energies[0])*27.21
-    string='%5i %10s%i %16.10f %12.8f %12.8f' % (istate+1,IToMult[m][0],s-(m<=2),e,de,fosc[-1])
-    print string
+      #else:
+        #dmx=dmy=dmz=0.
+        #fosc.append(0.)
+      if ezero!=0.0:
+        de=(e-ezero)*27.21
+      else:
+        de=(e-energies[0])*27.21
+      string='%5i %10s%02i %16.10f %12.8f %12.8f   %6.4f' % (istate+1,IToMult[ist[0]][0],ist[1]-(ist[0]<=2),e,de,fosc[-1],spin)
+      print string
+  else:
+    for istate in range(QMin['nmstates']):
+      m,s,ms=QMin['statemap'][istate+1]
+      if 2*ms+1!=m:
+        continue
+      e=QMout['h'][0][istate][istate].real
+      energies.append(e)
+      if m==1 and s>0:
+        dmx=QMout['dm'][0][istate][0].real
+        dmy=QMout['dm'][1][istate][0].real
+        dmz=QMout['dm'][2][istate][0].real
+        f=2./3.*(e-energies[0])*(dmx**2+dmy**2+dmz**2)
+        fosc.append(f)
+      else:
+        dmx=dmy=dmz=0.
+        fosc.append(0.)
+      if ezero!=0.0:
+        de=(e-ezero)*27.21
+      else:
+        de=(e-energies[0])*27.21
+      string='%5i %10s%02i %16.10f %12.8f %12.8f   %6.4f' % (istate+1,IToMult[m][0],s-(m<=2),e,de,fosc[-1],m)
+      print string
 
 
 
