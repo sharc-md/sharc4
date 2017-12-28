15d9fff81017b18faca64aca3811c4115cf03b06
Moritz Heindl <moritz@iron.itc.univie.ac.at>
2017-12-28 22:02:31 +0100

    changed factorial and laguerre polynomials

--- DEVELOPMENT/bin/wigner.py
+++ DEVELOPMENT/bin/wigner.py
@@ -565,50 +565,26 @@ file. Returns molecule and modes as the other function does.
 # ======================================================================================================================
 # ======================================================================================================================
 
-def factorialold(n):
-    """This function recursively calculates the factorial of n."""
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 1
-    else:
-        return n * factorial(n-1)
 
-def factorial(n,ndic):
-    """This function recursively calculates the factorial of n  and
-stores it in ndicif it is not stored in ndic already."""
-    if n not in ndic:
-      m,ndic =factorial(n-1,ndic)
-      ndic[n] = m *n
-      return ndic[n], ndic
-    else:
-      return ndic[n], ndic
-  #p=1.
-  #for i in range(n):
-    #p*=i+1
-  #return p
+def factorial(n,start):
+    """This function calculates the factorial of n."""
+    p=1.
+    for i in range(n):
+        if i >= start:
+            p*=i+1
+    return p
 
-def ana_laguerre(n, x,ndic):
+def ana_laguerre(n, x):
     """This function analytically calculates the value of the nth order
-Laguerre polynomial at point x. The demoniator limits this function to
-n below 99."""
+Laguerre polynomial at point x. Computational limitations restrict very high 
+excited vibrational states (above 170). However, the harmonic approximation 
+is no good approximation at these high-lying vibrational states!"""
     total = 0.
     for m in range(n+1):
-      entry = (-1.)**m*factorial(n,ndic)[0]/(factorial(n-m,ndic)[0]*(factorial(m,ndic)[0]**2))*(x**m)
+      entry = (-1.)**m*factorial(n,m)/(factorial(n-m,1)*(factorial(m,1)))*(x**m)
       total += entry
     return total
-
-def laguerre(n, x):
-    """This function recursively calculates the value of the nth order
-Laguerre polynomial at point x."""
-    if n == 0:
-        return 1.0
-    elif n == 1:
-        return 1.0 - x
-    else:
-        L = 1.0 / n * ( (2.0*(n-1.0) + 1.0 - x) * laguerre(n-1, x)
-                        - (n-1.0) * laguerre(n-2, x) )
-        return L
+        
 def determine_state(mode):
   """This function determines the vibrational state of the
 mode for the system at a certain temperature."""
@@ -653,7 +629,7 @@ mode for the system at a certain temperature."""
       break
 
 
-def wigner(Q, P, mode, ndic):
+def wigner(Q, P, mode):
     """This function calculates the Wigner distribution for
 a single one-dimensional harmonic oscillator.
 Q contains the dimensionless coordinate of the
@@ -667,19 +643,19 @@ The function returns a probability for this set of parameters."""
       #square of the factorial becomes to large to handle. Keep in mind,
       #that the harmonic approximation is most likely not valid at these
       #excited states
-      if n > 98:
+      if n > 150:
         if high_temp:
           n = -1
           print 'Highest considered vibrational state reached! Discarding this probability.'
         else:
-          print 'The calculated excited vibrational state for this normal mode exceeds the limit of the calculation.\nThe harmonic approximation is not valid for high vibrational states of low-frequency normal modes. The vibrational state ',n,' was set to 98. If you want to discard these states instead (due to oversampling of state nr 98), use the -T option.'
-          n = 98
+          print 'The calculated excited vibrational state for this normal mode exceeds the limit of the calculation.\nThe harmonic approximation is not valid for high vibrational states of low-frequency normal modes. The vibrational state ',n,' was set to 150. If you want to discard these states instead (due to oversampling of state nr 150), use the -T option.'
+          n = 150
     if n == 0: # vibrational ground state
         return (math.exp(-Q**2) * math.exp(-P**2), 0.)
     # TODO: what about n==-1 ??
     else: # vibrational excited state
         rhosquare = 2.0 * (P**2 + Q**2)
-        W = (-1.0)**n * ana_laguerre(n,rhosquare,ndic) \
+        W = (-1.0)**n * ana_laguerre(n,rhosquare) \
           * math.exp(-rhosquare/2.0)
         n=float(n)
         return (W,n)
@@ -709,7 +685,7 @@ for the first 11 vibrational states."""
     for Q in grid:
         outstring += '%6.2f' % Q
         for n in range(11):
-            outstring += ' % 12.8e' % laguerre(n, Q)
+            outstring += ' % 12.8e' % ana_laguerre(n, Q)
         outstring += '\n'
     outfile = open('laguerreplot.out', 'w')
     outfile.write(outstring)
@@ -1029,7 +1005,7 @@ weighted normal coordinates are then returned'''
 
 
 
-def sample_initial_condition(molecule, modes, ndic):
+def sample_initial_condition(molecule, modes):
   """This function samples a single initial condition from the
 modes and atomic coordinates by the use of a Wigner distribution.
 The first atomic dictionary in the molecule list contains also
@@ -1050,7 +1026,7 @@ Method is based on L. Sun, W. L. Hase J. Chem. Phys. 133, 044313
       random_Q = random.random()*6.0 - 3.0
       random_P = random.random()*6.0 - 3.0
       # calculate probability for this set of P and Q with Wigner distr.
-      probability = wigner(random_Q, random_P, mode, ndic)
+      probability = wigner(random_Q, random_P, mode)
       if probability[0]>1. or probability[0]<0.:
         if temperature == 0:
           print 'WARNING: wrong probability %f detected!' % (probability[0])
@@ -1157,14 +1133,11 @@ data given in 'molecule' and 'modes'. Output is returned
 as a list containing all initial condition objects."""
     print 'Sampling initial conditions'
     ic_list = []
-    ndic = {}
-    ndic[0] = 1
-    ndic[1] = 1
     width = 50
     idone = 0
     for i in range(1,amount+1): # for each requested initial condition
         # sample the initial condition
-        ic = sample_initial_condition(molecule, modes,ndic)
+        ic = sample_initial_condition(molecule, modes)
         ic_list.append(ic)
         idone += 1
         done = idone*width/(amount)
