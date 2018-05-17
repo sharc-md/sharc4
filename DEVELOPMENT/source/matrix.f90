6b50e3e057dce66ebb9c00b45236a68e93d0be3c
Sebastian Mai <sebastian.mai@univie.ac.at>
2018-05-17 09:16:03 +0200

    sharc.x source: added License header for release 2.0

--- DEVELOPMENT/source/matrix.f90
+++ DEVELOPMENT/source/matrix.f90
@@ -1,3 +1,26 @@
+!******************************************
+!
+!    SHARC Program Suite
+!
+!    Copyright (c) 2018 University of Vienna
+!
+!    This file is part of SHARC.
+!
+!    SHARC is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    SHARC is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    inside the SHARC manual.  If not, see <http://www.gnu.org/licenses/>.
+!
+!******************************************
+
 !> # Module MATRIX
 !>
 !> \author Sebastian Mai
@@ -600,9 +623,9 @@ subroutine zdiagonalize_and_project(n,H,U,Uold)
 
   ! diagonalise H
 !           Hinput=H
+!           call matwrite(n,H,6,'H','ES24.16E2')
   call zdiagonalize(n,H,U)
 !           call transform(n,Hinput,U,'utau')
-!           call matwrite(n,H,6,'H','F12.9')
 !           call matwrite(n,Hinput,6,'U^tHU','F12.9')
   do i=1,n
     eigv(i)=H(i,i)
@@ -868,7 +891,6 @@ do istep=1,nsubsteps
     Hold=SOold + (SO-SOold)*(istep-1)/nsubsteps
     if (printlevel>6) then
       write(u_print,*) 'dtsubstep=',dtsubstep
-
     endif
     call z_project_recursive(n, Hold, H, Uold_dummy, U, dtsubstep, printlevel, u_print)
 
