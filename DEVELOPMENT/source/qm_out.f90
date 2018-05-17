ddb2831cbea8ab618d554a6667854f86fd49b5fb
Sebastian Mai <sebastian.mai@univie.ac.at>
2018-05-17 09:16:41 +0200

    sharc.x : shifts degenerate diagonal elements by 1d-12

--- DEVELOPMENT/source/qm_out.f90
+++ DEVELOPMENT/source/qm_out.f90
@@ -1,3 +1,26 @@
+!******************************************
+!
+!    SHARC Program Suite
+!
+!    Copyright (c) 2018 University of Vienna
+!
+!    This file is part of SHARC.
+!
+!    SHARC is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    SHARC is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    inside the SHARC manual.  If not, see <http://www.gnu.org/licenses/>.
+!
+!******************************************
+
 !> # Module QM_OUT
 !>
 !> \author Sebastian Mai
@@ -207,6 +230,15 @@ subroutine get_hamiltonian(n, H_ss)
     write(0,*) 'Hamiltonian matrix has wrong format! nrow=',irow,'ncol=',icol
     stop 1
   endif
+
+  ! if degenerate diagonals, add tiny amount to avoid numerical problems
+  do icol=2,n
+    do irow=1,icol-1
+    if (H_ss(icol,icol)==H_ss(irow,irow)) then
+      H_ss(icol,icol)=H_ss(icol,icol)+1.d-12
+    endif
+    enddo
+  enddo
 
 endsubroutine
 
