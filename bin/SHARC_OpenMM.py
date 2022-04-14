b5c0706f0ed4bc0fba2212ce6a548a48a2270db0
Severin Polonius <severin.polonius@univie.ac.at>
2022-04-14 13:08:46 +0200

    cleaned up

--- bin/SHARC_OpenMM.py
+++ bin/SHARC_OpenMM.py
@@ -31,16 +31,15 @@
 
 # IMPORTS
 # external
-from matplotlib.pyplot import getp
 import numpy as np
 from openmm.app import Simulation, AmberPrmtopFile
-from openmm import VerletIntegrator, System, State, NonbondedForce, Platform, CustomIntegrator
-from openmm.unit import Quantity, bohr, kilocalories_per_mole, picosecond, angstrom, nanometer
+from openmm import System, State, NonbondedForce, Platform, CustomIntegrator
+from openmm.unit import Quantity, bohr, picosecond, nanometer
 
 # internal
 from SHARC_INTERFACE import INTERFACE
 from utils import *
-from constants import kcal_to_Eh as kcal_per_mole_to_Eh, au2a, au2eV
+from constants import au2a, kJpermol_to_Eh
 
 __author__ = 'Sebastian Mai and Severin Polonius'
 __version__ = '3.0'
@@ -48,7 +47,6 @@ versiondate = datetime.datetime(2022, 4, 8)
 
 changelogstring = '''
 '''
-np.set_printoptions(linewidth=400, formatter={'all': lambda x: str(x)})
 
 
 class OpenMM(INTERFACE):
@@ -75,36 +73,16 @@ class OpenMM(INTERFACE):
     def authors(self):
         return self._authors
 
-    # TODO: warnings for unsupported settings, i.e. nacs, socs etc
     def run(self):
-        # crd = [Quantity(Vec3(*x), unit=angstrom) for x in self._QMin['coords'] * au2a]
-        # print(crd[0])
         self.simulation.context.setPositions(Quantity(self._QMin['coords'], unit=bohr))
-        self.simulation.context.setVelocities(Quantity(np.zeros(self._QMin['coords'].shape), unit=nanometer/picosecond))
-        # print(self.simulation.context.getSystem())
-        # for _ in range(5):
-        #     results: State = self.simulation.context.getState(getPositions=True, getEnergy=True, getForces=True)
-        #     pos = results.getPositions(asNumpy=True)._value
-        #     forces: Quantity = results.getForces()._value
-        #     energy: Quantity = results.getPotentialEnergy()._value
-        #     self.simulation.step(1)
-        #     print(energy)
-        #     print(pos[:10, :])
-        #     print(forces[0])
-        #     print()
-        # self.simulation.step(1)
+        self.simulation.context.setVelocities(
+            Quantity(np.zeros(self._QMin['coords'].shape), unit=nanometer / picosecond)
+        )
         state: State = self.simulation.context.getState(getForces=True, getEnergy=True)
-        # pos = state.getPositions(asNumpy=True)._value
-        # forces: Quantity = state.getForces()._value
-        # energy: Quantity = state.getPotentialEnergy()._value
-        # print(energy)
-        # print(pos[:10, :])
-        # print(forces[0])
-        # print()
-        # # gradients: np.ndarray = -np.array(state.getForces(asNumpy=False).value_as_unit(kilocalories_per_mole/angstrom)) / 627.5094740631  # kJ/(mol*nm) -> Hartree/bohr 2625.4996394799 2625.499624258563
-        gradients: np.ndarray = -state.getForces(asNumpy=True) / 2625.4996394799 * 0.1 * au2a # kJ/(mol*nm) -> Hartree/bohr
-        # print(np.sqrt(np.sum(gradients**2)/3/gradients.shape[0])/au2a * au2eV )
-        energy: float = state.getPotentialEnergy()._value / 2625.4996394799  # kJ/mol -> Hartree
+        gradients: np.ndarray = -state.getForces(
+            asNumpy=True
+        ) / kJpermol_to_Eh * 0.1 * au2a    # kJ/(mol*nm) -> Hartree/bohr
+        energy: float = state.getPotentialEnergy()._value / kJpermol_to_Eh    # kJ/mol -> Hartree
 
         self._QMout['h'] = [[energy]]
         self._QMout['grad'] = [gradients.tolist()]
@@ -132,20 +110,19 @@ class OpenMM(INTERFACE):
     def setup_run(self):
         QMin = self._QMin
         prmtop = AmberPrmtopFile(QMin['prmtop'])
-        # standard params
+
+        # standard params http://docs.openmm.org/latest/api-python/generated/openmm.app.amberprmtopfile.AmberPrmtopFile.html#openmm.app.amberprmtopfile.AmberPrmtopFile.createSystem
         # nonbondedMethod=ff.NoCutoff, nonbondedCutoff=1.0*u.nanometer, constraints=None, rigidWater=True, implicitSolvent=None, implicitSolventSaltConc=0.0*(u.moles/u.liter), implicitSolventKappa=None, temperature=298.15*u.kelvin, soluteDielectric=1.0, solventDielectric=78.5, removeCMMotion=True, hydrogenMass=None, ewaldErrorTolerance=0.0005, switchDistance=0.0*u.nanometer, gbsaModel='ACE'
         system: System = prmtop.createSystem(nonbondedCutoff='NoCutoff', rigidWater=False, removeCMMotion=False)
-        # integrator = VerletIntegrator(0.0005)
+
+        # 'Do nothing' integrator
         integrator = CustomIntegrator(0.0005)
-        # integrator.addPerDofVariable("a", 0)
-        # integrator.addUpdateContextState()
-        # integrator.addComputePerDof("x", "x+dt*v+0.5*a*dt*dt")
-        # integrator.addComputePerDof("v", "v+0.5*dt*(f/m+a)")
-        # integrator.addComputePerDof("a", "f/m")
         integrator.addComputePerDof("x", "x")
         integrator.addComputePerDof("v", "v")
+
         platform = Platform.getPlatformByName('CPU')
         properties = {'Threads': str(QMin['ncpu'])}
+
         self.simulation = Simulation(prmtop.topology, system, integrator, platform, platformProperties=properties)
         nonbonded = [f for f in system.getForces() if isinstance(f, NonbondedForce)][0]
         self._charges = []
@@ -158,6 +135,7 @@ class OpenMM(INTERFACE):
 
         if QMin['states'] != [1]:
             raise Error('MM calculation only implemented for the S0 state! "states" has to be "1"')
+
         # prepare savedir
         if not os.path.isdir(QMin['savedir']):
             mkdir(QMin['savedir'])
@@ -170,9 +148,6 @@ class OpenMM(INTERFACE):
         not_allowed = {'soc', 'dmdr', 'socdr', 'ion', 'nacdr', 'theodore'}
         if not QMin.keys().isdisjoint(not_allowed):
             raise Error('Cannot perform tasks: {}'.format(' '.join(QMin.keys() & not_allowed)), 13)
-
-        # if 'grad' in QMin:
-        #     print('Warning: gradient only calculated for first singlet!')
 
 
 if __name__ == "__main__":
