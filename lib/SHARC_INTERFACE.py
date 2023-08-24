940dd4cf973cc5229d8b62c23b1e35ecb3693f40
smausenberger <sascha.mausenberger@univie.ac.at>
2023-08-24 15:11:02 +0200

    fix import

--- lib/SHARC_INTERFACE.py
+++ lib/SHARC_INTERFACE.py
@@ -42,7 +42,7 @@ from constants import ATOMCHARGE, FROZENS, BOHR_TO_ANG
 from logger import log as logging
 from qmin import QMin
 from qmout import QMout
-from utils import readfile, clock, parse_xyz, itnmstates, expand_path
+from utils import readfile, clock, parse_xyz, itnmstates, expand_path, Error
 
 all_features = {
     "h",
