b02e0c8574453c596884dbf16e6c0c597e2c1436
smausenberger <sascha.mausenberger@univie.ac.at>
2023-09-07 16:48:58 +0200

    initial commit of new orca interface

---
+++ bin/SHARC_ORCA_new.py
@@ -0,0 +1,376 @@
+import datetime
+import os
+import re
+import subprocess as sp
+from io import TextIOWrapper
+from typing import Optional
+import struct
+
+from qmin import QMin
+from SHARC_ABINITIO import SHARC_ABINITIO
+from utils import expand_path, itmult
+
+AUTHORS = ""
+VERSION = ""
+VERSIONDATE = datetime.datetime(2023, 8, 29)
+NAME = "ORCA"
+DESCRIPTION = ""
+
+CHANGELOGSTRING = """
+"""
+
+all_features = set(
+    [
+        "h",
+        "dm",
+        "soc",
+        "theodore",
+        "grad",
+        "ion",
+        "overlap",
+        "phases",
+        # raw data request
+        "basis_set",
+        "wave_functions",
+        "density_matrices",
+    ]
+)
+
+
+class SHARC_ORCA(SHARC_ABINITIO):
+    """
+    SHARC interface for ORCA
+    """
+
+    _version = VERSION
+    _versiondate = VERSIONDATE
+    _authors = AUTHORS
+    _changelogstring = CHANGELOGSTRING
+    _name = NAME
+    _description = DESCRIPTION
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Add resource keys
+        self.QMin.resources["orcadir"] = None
+        self.QMin.resources["schedule_scaling"] = 0.9
+        self.QMin.resources["orcadir"] = None
+        self.QMin.resources["orcaversion"] = None
+        self.QMin.resources["wfoverlap"] = None
+        self.QMin.resources["wfthres"] = None
+        self.QMin.resources["numfrozcore"] = None
+        self.QMin.resources["numocc"] = None
+
+        self.QMin.resources.types["orcadir"] = str
+        self.QMin.resources.types["orcaversion"] = tuple
+        self.QMin.resources.types["wfoverlap"] = str
+        self.QMin.resources.types["wfthres"] = float
+        self.QMin.resources.types["numfrozcore"] = int
+        self.QMin.resources.types["numocc"] = int
+        self.QMin.resources.types["schedule_scaling"] = float
+
+        # Add template keys
+        self.QMin.template["no_tda"] = False
+        self.QMin.template["unrestricted_triplets"] = False
+        self.QMin.template["picture_change"] = False
+        self.QMin.template["basis"] = "6-31G"
+        self.QMin.template["auxbasis"] = None
+        self.QMin.template["functional"] = "PBE"
+        self.QMin.template["dispersion"] = None
+        self.QMin.template["grid"] = None
+        self.QMin.template["gridx"] = None
+        self.QMin.template["gridxc"] = None
+        self.QMin.template["ri"] = None
+        self.QMin.template["scf"] = None
+        self.QMin.template["keys"] = None
+        self.QMin.template["paste_input_file"] = None
+        self.QMin.template["frozen"] = -1
+        self.QMin.template["maxiter"] = 700
+        self.QMin.template["hfexchange"] = -1.0
+        self.QMin.template["intacc"] = -1.0
+
+        self.QMin.template.types["no_tda"] = bool
+        self.QMin.template.types["unrestricted_triplets"] = bool
+        self.QMin.template.types["picture_change"] = bool
+        self.QMin.template.types["basis"] = str
+        self.QMin.template.types["auxbasis"] = str
+        self.QMin.template.types["functional"] = str
+        self.QMin.template.types["dispersion"] = str
+        self.QMin.template.types["grid"] = str
+        self.QMin.template.types["gridx"] = str
+        self.QMin.template.types["gridxc"] = str
+        self.QMin.template.types["ri"] = str
+        self.QMin.template.types["scf"] = str
+        self.QMin.template.types["keys"] = (str, list)
+        self.QMin.template.types["paste_input_file"] = str
+        self.QMin.template.types["frozen"] = int
+        self.QMin.template.types["maxiter"] = int
+        self.QMin.template.types["hfexchange"] = float
+        self.QMin.template.types["intacc"] = float
+        # no range_sep_settings, can be done with paste_input_file
+
+    @staticmethod
+    def version() -> str:
+        return SHARC_ORCA._version
+
+    @staticmethod
+    def versiondate() -> datetime.datetime:
+        return SHARC_ORCA._versiondate
+
+    @staticmethod
+    def changelogstring() -> str:
+        return SHARC_ORCA._changelogstring
+
+    @staticmethod
+    def authors() -> str:
+        return SHARC_ORCA._authors
+
+    @staticmethod
+    def name() -> str:
+        return SHARC_ORCA._name
+
+    @staticmethod
+    def description() -> str:
+        return SHARC_ORCA._description
+
+    @staticmethod
+    def about() -> str:
+        return f"{SHARC_ORCA._name}\n{SHARC_ORCA._description}"
+
+    def get_features(self, KEYSTROKES: Optional[TextIOWrapper] = None) -> set[str]:
+        """return availble features
+
+        ---
+        Parameters:
+        KEYSTROKES: object as returned by open() to be used with question()
+        """
+        return all_features
+
+    def get_infos(
+        self, INFOS: dict, KEYSTROKES: Optional[TextIOWrapper] = None
+    ) -> dict:
+        """prepare INFOS obj
+
+        ---
+        Parameters:
+        INFOS: dictionary with all previously collected infos during setup
+        KEYSTROKES: object as returned by open() to be used with question()
+        """
+        return INFOS
+
+    def create_restart_files(self):
+        pass
+
+    def execute_from_qmin(self, workdir: str, qmin: QMin):
+        """
+        Erster Schritt, setup_workdir ( inputfiles schreiben, orbital guesses kopieren, xyz, pc)
+        Programm aufrufen (z.b. run_program)
+        checkstatus(), check im workdir ob rechnung erfolgreich
+            if success: pass
+            if not: try again or return error
+        postprocessing of workdir files (z.b molden file erzeugen, stripping)
+        """
+
+    def getQMout(self):
+        pass
+
+    def prepare(self, INFOS: dict, dir_path: str):
+        "setup the calculation in directory 'dir'"
+        return
+
+    def printQMout(self) -> None:
+        super().writeQMout()
+
+    def print_qmin(self) -> None:
+        pass
+
+    def read_resources(
+        self, resources_file: str, kw_whitelist: Optional[list[str]] = None
+    ) -> None:
+        super().read_resources(resources_file, kw_whitelist)
+
+        # LD PATH???
+        if "orcadir" not in self.QMin.resources:
+            raise ValueError("orcadir has to be set in resource file!")
+
+        self.QMin.resources["orcadir"] = expand_path(self.QMin.resources["orcadir"])
+        self.log.debug(f'orcadir set to {self.QMin.resources["orcadir"]}')
+
+        self.QMin.resources["orcaversion"] = SHARC_ORCA.get_orca_version(
+            self.QMin.resources["orcadir"]
+        )
+        self.log.info(
+            f'Detected ORCA version {".".join(str(i) for i in self.QMin.resources["orcaversion"])}'
+        )
+
+        if self.QMin.resources["orcaversion"] < (5, 0):
+            raise ValueError(
+                "This version of the SHARC-ORCA interface is only compatible to Orca 5.0 or higher!"
+            )
+
+    def read_requests(self, requests_file: str = "QM.in") -> None:
+        super().read_requests(requests_file)
+
+        for req, val in self.QMin.requests.items():
+            if val and req not in all_features:
+                raise ValueError(f"Found unsupported request {req}.")
+
+    def read_template(self, template_file: str) -> None:
+        super().read_template(template_file)
+
+    def remove_old_restart_files(self, retain: int = 5) -> None:
+        """
+        Garbage collection after runjobs()
+        """
+
+    def run(self) -> None:
+        """
+        request & other logic
+            requestmaps anlegen -> DONE IN SETUP_INTERFACE
+            pfade für verschiedene orbital restart files
+        make schedule
+        runjobs()
+        run_wfoverlap (braucht input files)
+        run_theodore
+        save directory handling
+        """
+
+    def setup_interface(self) -> None:
+        """
+        Setup remaining maps (ionmap, gsmap) and build jobs dict
+        """
+        super().setup_interface()
+
+        if (
+            not self.QMin.template["unrestricted_triplets"]
+            and len(self.QMin.molecule["states"]) >= 3
+            and self.QMin.molecule["states"][2] > 0
+        ):
+            self.log.debug("Unrestricted triplets requested, setup states_to_do")
+            self.QMin.control["states_to_do"][0] = max(
+                self.QMin.molecule["states"][0], 1
+            )
+            req = max(
+                self.QMin.molecule["states"][0] - 1, self.QMin.molecule["states"][2]
+            )
+            self.QMin.control["states_to_do"][0] = req + 1
+            self.QMin.control["states_to_do"][2] = req
+
+        self._build_jobs()
+        # Setup multmap
+        self.log.debug("Building multmap")
+        self.QMin.maps["multmap"] = {}
+        for ijob, job in self.QMin.control["jobs"].items():
+            for imult in job["mults"]:
+                self.QMin.maps["multmap"][imult] = ijob
+            self.QMin.maps["multmap"][-(ijob)] = job["mults"]
+        self.QMin.maps["multmap"][1] = 1
+
+        # Setup ionmap
+        if self.QMin.requests["ion"]:
+            self.log.debug("Building ionmap")
+            self.QMin.maps["ionmap"] = []
+            for m1 in itmult(self.QMin.molecule["states"]):
+                job1 = self.QMin.maps["multmap"][m1]
+                el1 = self.QMin.maps["chargemap"][m1]
+                for m2 in itmult(self.QMin.molecule["states"]):
+                    if m1 >= m2:
+                        continue
+                    job2 = self.QMin.maps["multmap"][m2]
+                    el2 = self.QMin.maps["chargemap"][m2]
+                    if abs(m1 - m2) == 1 and abs(el1 - el2) == 1:
+                        self.QMin.maps["ionmap"].append((m1, job1, m2, job2))
+
+        # Setup gsmap
+        self.log.debug("Building gsmap")
+        self.QMin.maps["gsmap"] = {}
+        for i in range(self.QMin.molecule["nmstates"]):
+            m1, _, ms1 = tuple(self.QMin.maps["statemap"][i + 1])
+            gs = (m1, 1, ms1)
+            job = self.QMin.maps["multmap"][m1]
+            if m1 == 3 and self.QMin.control["jobs"][job]["restr"]:
+                gs = (1, 1, 0.0)
+            for j in range(self.QMin.molecule["nmstates"]):
+                if tuple(self.QMin.maps["statemap"][j + 1]) == gs:
+                    break
+                self.QMin.maps["gsmap"][i + 1] = j + 1
+
+    def _build_jobs(self) -> None:
+        """
+        Build job dictionary from states_to_do
+        """
+        self.log.debug("Building job map.")
+        jobs = {}
+        for idx, val in enumerate(self.QMin.control["states_to_do"]):
+            if val == 0:
+                continue
+            match idx:
+                case 0:
+                    jobs[1] = {"mults": [1], "restr": True}
+                case 1:
+                    jobs[2] = {"mults": [2], "restr": False}
+                case 2:
+                    if self.QMin.template["unrestricted_triplets"] and 1 in jobs:
+                        jobs[1]["mults"].append(3)
+                    else:
+                        jobs[3] = {"mults": [3], "restr": False}
+                case _:
+                    jobs[idx + 5] = {"mults": [idx + 5], "restr": False}
+
+        self.QMin.control["jobs"] = jobs
+
+    def write_step_file(self) -> None:
+        super().write_step_file()
+
+    def get_dets_from_cis(self, cis_path: str):
+        """
+        Parse ORCA.cis file from WORKDIR
+        """
+        cis_path = (
+            cis_path if os.path.isfile(cis_path) else os.path.join(cis_path, "ORCA.cis")
+        )
+        self.QMin.control["jobid"] = 1  # TODO: REMOVE AFTER DEBUGGING!!!
+        restricted = self.QMin.control["jobs"][self.QMin.control["jobid"]]["restr"]
+
+        with open(cis_path, "rb") as cis_file:
+            cis_file.read(4)
+            header = [struct.unpack("i", cis_file.read(4))[0] for i in range(8)]
+
+            # Extract information from header
+            # Number occupied A/B and number virtual A/B
+            noa = header[1] - header[0] + 1
+            nva = header[3] - header[2] + 1
+            nob = header[5] - header[4] + 1 if not restricted else noa
+            nvb = header[7] - header[6] + 1 if not restricted else nva
+            self.log.debug(
+                f"Extracting header from CIS file, NOA: {noa}, NVA: {nva}, NOB: {nob}, NVB: {nvb}"
+            )
+
+    @staticmethod
+    def get_orca_version(path: str) -> tuple[int, ...]:
+        """
+        Get ORCA version number of given path
+        """
+        string = os.path.join(path, "orca") + " nonexisting"
+        with sp.Popen(string, shell=True, stdout=sp.PIPE, stderr=sp.PIPE) as proc:
+            comm = proc.communicate()[0].decode()
+            if not comm:
+                raise ValueError("ORCA version not found!")
+            version = re.findall(r"Program Version (\d.\d.\d)", comm)[0].split(".")
+            return tuple(int(i) for i in version)
+
+
+if __name__ == "__main__":
+    test = SHARC_ORCA(loglevel=10)
+    test.setup_mol("QM.in")
+    test.read_resources(
+        "ORCA.resources", kw_whitelist=["theodore_prop", "theodore_fragment"]
+    )
+    test.read_template("ORCA.template")
+    test.read_requests("QM.in")
+    test.setup_interface()
+    test.get_dets_from_cis(
+        "/user/mai/Documents/CoWorkers/FelixProche/full/orca.cis"
+    )
+    # print(test.QMin)
