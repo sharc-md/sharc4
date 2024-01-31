62ecf94a37c865ce96196d1e8a41bf2e35048524
smausenberger <sascha.mausenberger@univie.ac.at>
2024-01-31 13:41:32 +0100

    implement task generation and tests

--- tests/interface/test_molcas.py
+++ tests/interface/test_molcas.py
@@ -24,3 +24,67 @@ def test_molcasversion1():
     for i in tests:
         with pytest.raises((ValueError, FileNotFoundError)):
             SHARC_MOLCAS().get_molcas_version(i)
+
+
+def test_gettasks():
+    tests = [
+        (
+            os.path.join(PATH, "inputs/molcas/tasks/QM1.in"),
+            os.path.join(PATH, "inputs/molcas/tasks/template1"),
+            [
+                ["gateway"],
+                ["seward"],
+                ["rasscf", 1, 4, False, False],
+                ["copy", "MOLCAS.JobIph", "MOLCAS.1.JobIph"],
+                ["copy", "MOLCAS.RasOrb", "MOLCAS.1.RasOrb"],
+                ["copy", "MOLCAS.rasscf.molden", "MOLCAS.1.molden"],
+                ["link", "MOLCAS.1.JobIph", "JOBOLD"],
+                ["rasscf", 1, 4, True, False],
+                ["mclr", 0.0001, "sala=2"],
+                ["alaska"],
+                ["link", "MOLCAS.1.JobIph", "JOBOLD"],
+                ["rasscf", 1, 4, True, False],
+                ["mclr", 0.0001, "sala=1"],
+                ["alaska"],
+                ["link", "MOLCAS.1.JobIph", "JOBOLD"],
+                ["rasscf", 1, 4, True, False],
+                ["mclr", 0.0001, "sala=3"],
+                ["alaska"],
+                ["link", "MOLCAS.1.JobIph", "JOBOLD"],
+                ["rasscf", 1, 4, True, False],
+                ["mclr", 0.0001, "sala=4"],
+                ["alaska"],
+                ["link", "MOLCAS.1.JobIph", "JOB001"],
+                ["rassi", "dm", [4]],
+                ["rasscf", 2, 2, False, False],
+                ["copy", "MOLCAS.JobIph", "MOLCAS.2.JobIph"],
+                ["copy", "MOLCAS.RasOrb", "MOLCAS.2.RasOrb"],
+                ["copy", "MOLCAS.rasscf.molden", "MOLCAS.2.molden"],
+                ["link", "MOLCAS.2.JobIph", "JOBOLD"],
+                ["rasscf", 2, 2, True, False],
+                ["mclr", 0.0001, "sala=1"],
+                ["alaska"],
+                ["link", "MOLCAS.2.JobIph", "JOBOLD"],
+                ["rasscf", 2, 2, True, False],
+                ["mclr", 0.0001, "sala=2"],
+                ["alaska"],
+                ["link", "MOLCAS.2.JobIph", "JOB001"],
+                ["rassi", "dm", [2]],
+                ["link", "MOLCAS.1.JobIph", "JOB001"],
+                ["link", "MOLCAS.2.JobIph", "JOB002"],
+                ["rassi", "soc", [4, 2]],
+            ],
+        )
+    ]
+
+    for qmin, templ, ref in tests:
+        test_interface = SHARC_MOLCAS()
+        test_interface.setup_mol(qmin)
+        test_interface.read_template(templ)
+        test_interface.QMin.save["step"] = 0
+        test_interface._read_resources = True
+        test_interface.read_requests(qmin)
+        test_interface.setup_interface()
+
+        tasks = test_interface._gen_tasklist(test_interface.QMin)
+        assert tasks == ref
