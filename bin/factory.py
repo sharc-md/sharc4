60f8db1a80b10cd3730e87be58d77b15e7423da8
Sebastian Mai <sebastian.mai@univie.ac.at>
2024-10-04 08:30:23 +0200

    factory: fixed factory() function to work with flexibly messages

--- bin/factory.py
+++ bin/factory.py
@@ -57,38 +57,38 @@ def get_available_interfaces() -> list[tuple[str, Union[SHARC_INTERFACE, str]]]:
             mod = import_module(interface_name)
         except TypeError as e:
             log.debug(f"{interface_name} could not be imported (not a package)\n\t{e}")
-            interfaces.append((interface_name, "(Not Available!)"))
+            interfaces.append((interface_name, "(Not Available!)", False))
             continue
         except (ModuleNotFoundError, ImportError) as e:
             log.debug(f"{interface_name} could not be imported (missing dependencies)\n\t{e}")
-            interfaces.append((interface_name, "(Not Available!)"))
+            interfaces.append((interface_name, "(Not Available!)", False))
             continue
 
         try:
             interface = getattr(mod, interface_name)
         except AttributeError as e:
             log.debug(f"class {interface_name} not found in {mod}\n\t{e}")
-            interfaces.append((interface_name, "(Not Available!)"))
+            interfaces.append((interface_name, "(Not Available!)", False))
             continue
 
         if issubclass(interface, SHARC_OLD):
             log.debug(f"class {interface_name} in {mod} is a legacy class")
-            interfaces.append((interface_name, "(Not Available! Use SHARC_LEGACY to work with this interface)"))
+            interfaces.append((interface_name, "(Not Available! Use SHARC_LEGACY to work with this interface)", False))
             continue
 
         if type(interface) == str or not issubclass(interface, SHARC_INTERFACE):
             log.debug(f"class {interface_name} in {mod} is not derived from 'SHARC_INTERFACE'")
-            interfaces.append((interface_name, "(Not Available!)"))
+            interfaces.append((interface_name, "(Not Available!)", False))
             continue
 
-        interfaces.append((interface_name, interface))
+        interfaces.append((interface_name, interface, True))
     log.debug(interfaces)
     AVAILABLE_INTERFACES = interfaces[:]
     return interfaces
 
 
 def factory(name: str) -> SHARC_INTERFACE:
-    available_interfaces = [i[1] for i in get_available_interfaces() if i[1] != "(Not Available!)"]
+    available_interfaces = [i[1] for i in get_available_interfaces() if i[2] ]
     names = [i.__name__.split("_", maxsplit=1)[1] for i in available_interfaces]
     log.debug(f"{available_interfaces}\n{names}")
     try:
