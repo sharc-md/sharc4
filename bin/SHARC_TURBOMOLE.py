08d1bbd89f9e9b4a3ba83da23298b7b2b412ae1d
smausenberger <sascha.mausenberger@univie.ac.at>
2024-10-11 13:04:53 +0200

    fix cc2 energy parsing bug

--- bin/SHARC_TURBOMOLE.py
+++ bin/SHARC_TURBOMOLE.py
@@ -1067,7 +1067,7 @@ class SHARC_TURBOMOLE(SHARC_ABINITIO):
             # Extract energy values
             energies += re.findall(r"-?\d+\.\d{7}", raw_energies[0])
         return (
-            np.asarray(energies, dtype=np.complex128) + float(gs_energy[0]),
+            np.asarray(energies, dtype=np.complex128) + float(gs_energy[-1]),
             re.findall(r"(\d+\.\d{2})\s", raw_energies[0])[1::2],
         )
 
