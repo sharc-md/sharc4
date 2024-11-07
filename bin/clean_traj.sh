3c5d350955ca63c1229b675b8fc64a71866bc76f
Sebastian Mai <sebastian.mai@univie.ac.at>
2024-11-07 09:31:53 +0100

    Added clean_traj.sh

---
+++ bin/clean_traj.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if the current directory starts with "TRAJ_"
+if [[ $(basename "$PWD") != TRAJ_* ]]; then
+    echo "Not in a TRAJ_* directory. Exiting without deleting anything."
+    exit 1
+fi
+
+echo "In TRAJ_* directory. Proceeding with deletions..."
+
+# Delete files with prefixes output.* and restart.*
+rm -v output.* restart.*
+
+# Delete empty indicator files
+rm -v STOP CRASHED DONT_ANALYZE RUNNING DEAD
+
+# Delete the file restart/STEP
+rm -v restart/STEP
+
+# Delete all files in restart/ that end with ".<integer>"
+find restart/ -type f -name '*.[0-9]*' -exec rm -v {} +
+
+# Recursively delete all files in QM/ that start with QM.
+find QM/ -type f -name 'QM.*' -exec rm -v {} +
+
+echo "Deletion completed."
+
