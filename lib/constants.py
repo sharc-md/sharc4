2e3710598d22c18d1db5fd41b9f3e0611f2c755c
Sebastian Mai <sebastian.mai@univie.ac.at>
2024-11-08 08:24:52 +0100

    constants.py: Changed some of the dictionaries

--- lib/constants.py
+++ lib/constants.py
@@ -31,9 +31,11 @@ U_TO_AMU           = ram2au
 BOHR_TO_ANG        = const.physical_constants["Bohr radius"][0]*1E10
 AMBERVEL_TO_AU     = (const.calorie*1E3/const.Avogadro/const.physical_constants["atomic mass constant"][0])**(0.5)/ \
                      (const.physical_constants["Bohr radius"][0]/const.physical_constants["atomic unit of time"][0])  # https://ambermd.org/Questions/units.html
-# TODO: go higher (see old ORCA interface
-# TODO: use some list comprehension to get the reversed half of the dictionary
-IToMult = {
+
+
+
+# Generate IToMult
+base_names = {
     1: 'Singlet',
     2: 'Doublet',
     3: 'Triplet',
@@ -41,28 +43,23 @@ IToMult = {
     5: 'Quintet',
     6: 'Sextet',
     7: 'Septet',
-    8: 'Octet',
-    9: '9-et',   
-    10: '10-et', 
-    11: '11-et', 
-    12: '12-et', 
-    13: '13-et', 
-    14: '14-et', 
-    15: '15-et', 
-    'Singlet': 1,
-    'Doublet': 2,
-    'Triplet': 3,
-    'Quartet': 4,
-    'Quintet': 5,
-    'Sextet': 6,
-    'Septet': 7,
-    'Octet': 8
+    8: 'Octet'
 }
+highest_index = 21
+IToMult = {}
+for i in range(1, highest_index + 1):
+    # Use the special names if they exist, otherwise use the "N-et" format
+    mult_name = base_names.get(i, f"{i}-et")
+    IToMult[i] = mult_name
+    IToMult[mult_name] = i
+
 
 # hash table for conversion of polarisations to the keywords
 # TODO: use some list comprehension to get the reversed half of the dictionary
 IToPol = {0: 'X', 1: 'Y', 2: 'Z', 'X': 0, 'Y': 1, 'Z': 2}
 
+
+# Atomic numbers
 NUMBERS = {
     'H': 1,
     'He': 2,
@@ -184,6 +181,8 @@ NUMBERS = {
     'Og': 118
 }
 ATOMCHARGE = NUMBERS
+IAn2AName = {v: k for k, v in NUMBERS.items()}
+
 # Number of frozen core orbitals
 FROZENS = {
     'H': 0,
@@ -305,127 +304,8 @@ FROZENS = {
     'Ts': 50,
     'Og': 50
 }
-# This could be automatically obtained as inversion of NUMBERS
-IAn2AName = {
-    1: "H",
-    2: "He",
-    3: "Li",
-    4: "Be",
-    5: "B",
-    6: "C",
-    7: "N",
-    8: "O",
-    9: "F",
-    10: "Ne",
-    11: "Na",
-    12: "Mg",
-    13: "Al",
-    14: "Si",
-    15: "P",
-    16: "S",
-    17: "Cl",
-    18: "Ar",
-    19: "K",
-    20: "Ca",
-    21: "Sc",
-    22: "Ti",
-    23: "V",
-    24: "Cr",
-    25: "Mn",
-    26: "Fe",
-    27: "Co",
-    28: "Ni",
-    29: "Cu",
-    30: "Zn",
-    31: "Ga",
-    32: "Ge",
-    33: "As",
-    34: "Se",
-    35: "Br",
-    36: "Kr",
-    37: "Rb",
-    38: "Sr",
-    39: "Y",
-    40: "Zr",
-    41: "Nb",
-    42: "Mo",
-    43: "Tc",
-    44: "Ru",
-    45: "Rh",
-    46: "Pd",
-    47: "Ag",
-    48: "Cd",
-    49: "In",
-    50: "Sn",
-    51: "Sb",
-    52: "Te",
-    53: "I",
-    54: "Xe",
-    55: "Cs",
-    56: "Ba",
-    57: "La",
-    58: "Ce",
-    59: "Pr",
-    60: "Nd",
-    61: "Pm",
-    62: "Sm",
-    63: "Eu",
-    64: "Gd",
-    65: "Tb",
-    66: "Dy",
-    67: "Ho",
-    68: "Er",
-    69: "Tm",
-    70: "Yb",
-    71: "Lu",
-    72: "Hf",
-    73: "Ta",
-    74: "W",
-    75: "Re",
-    76: "Os",
-    77: "Ir",
-    78: "Pt",
-    79: "Au",
-    80: "Hg",
-    81: "Tl",
-    82: "Pb",
-    83: "Bi",
-    84: "Po",
-    85: "At",
-    86: "Rn",
-    87: "Fr",
-    88: "Ra",
-    89: "Ac",
-    90: "Th",
-    91: "Pa",
-    92: "U",
-    93: "Np",
-    94: "Pu",
-    95: "Am",
-    96: "Cm",
-    97: "Bk",
-    98: "Cf",
-    99: "Es",
-    100: "Fm",
-    101: "Md",
-    102: "No",
-    103: "Lr",
-    104: "Rf",
-    105: "Db",
-    106: "Sg",
-    107: "Bh",
-    108: "Hs",
-    109: "Mt",
-    110: "Ds",
-    111: "Rg",
-    112: "Cn",
-    113: "Nh",
-    114: "Fl",
-    115: "Mc",
-    116: "Lv",
-    117: "Ts",
-    118: "Og"
-}
+
+
 # Atomic Weights of the most common isotopes
 # From https://chemistry.sciences.ncsu.edu/msf/pdf/IsotopicMass_NaturalAbundance.pdf
 MASSES = {
