41dc3bddc9ffcfdab5bef5ccfa0661b5537872ef
smausenberger <sascha.mausenberger@univie.ac.at>
2025-01-16 14:26:29 +0100

    update description

--- bin/SHARC_SPAINN.py
+++ bin/SHARC_SPAINN.py
@@ -15,7 +15,7 @@ AUTHORS = "Sascha Mausenberger"
 VERSION = "4.0"
 VERSIONDATE = datetime.datetime(2024, 10, 15)
 NAME = "SPAINN"
-DESCRIPTION = "FAST interface for SPaiNN"
+DESCRIPTION = "     FAST interface for SPaiNN"
 
 CHANGELOGSTRING = """
 """
