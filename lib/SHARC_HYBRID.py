7a876be4c3282c7d3998fe540aa0160d6f987306
Sebastian Mai <sebastian.mai@univie.ac.at>
2025-03-07 10:38:58 +0100

    Changed copyright notice on lib/

--- lib/SHARC_HYBRID.py
+++ lib/SHARC_HYBRID.py
@@ -4,7 +4,7 @@
 #
 #    SHARC Program Suite
 #
-#    Copyright (c) 2024 University of Vienna
+#    Copyright (c) 2025 University of Vienna
 #
 #    This file is part of SHARC.
 #
@@ -22,6 +22,7 @@
 #    inside the SHARC manual.  If not, see <http://www.gnu.org/licenses/>.
 #
 # ******************************************
+
 
 import asyncio
 import inspect
