16ba5226a8b6161c01d4b662923b43c6affddfd1
smausenberger <sascha.mausenberger@univie.ac.at>
2025-05-05 14:50:00 +0200

    add more requests to qmout tests

--- tests/interface/test_qmout.py
+++ tests/interface/test_qmout.py
@@ -10,7 +10,14 @@ def test_read_qmout():
     This test is to check if QM.out files can be loaded.
     No validations done here!
     """
-    tests = [os.path.join(PATH, "qmout_ion.out"), os.path.join(PATH, "qmout_theodore.out")]
+    tests = [
+        os.path.join(PATH, "qmout_ion.out"),
+        os.path.join(PATH, "qmout_theodore.out"),
+        os.path.join(PATH, "qmout_overlap.out"),
+        os.path.join(PATH, "qmout_notes.out"),
+        os.path.join(PATH, "qmout_nacdr.out"),
+        os.path.join(PATH, "qmout_dens.out"),
+    ]
 
     for test in tests:
         QMout(test)
