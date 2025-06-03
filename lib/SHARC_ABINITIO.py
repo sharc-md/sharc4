af39f7bfa1c9dc0881411088d1e99aeee53e5e8f
smausenberger <sascha.mausenberger@univie.ac.at>
2025-06-03 15:29:23 +0200

    fix resp_beta parsing

--- lib/SHARC_ABINITIO.py
+++ lib/SHARC_ABINITIO.py
@@ -162,6 +162,7 @@ class SHARC_ABINITIO(SHARC_INTERFACE):
             self.QMin.resources["wfoverlap"] = expand_path(self.QMin.resources["wfoverlap"])
         if self.QMin.resources["theodore_fragment"]:
             self.QMin.resources["theodore_fragment"] = convert_list(self.QMin.resources["theodore_fragment"])
+        self.QMin.resources["resp_betas"] = convert_list(self.QMin.resources["resp_betas"], float)
         if self.QMin.resources["resp_vdw_radii"]:
             self.QMin.resources["resp_vdw_radii"] = convert_list(self.QMin.resources["resp_vdw_radii"], float)
         if self.QMin.resources["resp_vdw_radii_symbol"]:
