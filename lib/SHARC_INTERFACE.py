c160538b91ef891236a304d791596a0a1f6f016e
smausenberger <sascha.mausenberger@univie.ac.at>
2025-06-17 12:23:12 +0200

    fix init args

--- lib/SHARC_INTERFACE.py
+++ lib/SHARC_INTERFACE.py
@@ -89,10 +89,12 @@ class SHARC_INTERFACE(ABC):
 
     def __init__(
         self,
+        *args,
         persistent=False,
         logname: str | None = None,
         logfile: str | None = None,
         loglevel: int = loglevel,
+        **kwargs,
     ):
         # all the output from the calculation will be stored here
         self.QMout = QMout()
