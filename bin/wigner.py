f36eedfd148a37c1e749ff8d5f8c43f965b5d153
Marco Romanelli <marcor@manganese.itc.univie.ac.at>
2025-08-07 10:26:29 +0200

    Fixed debugging for wigner.py matrices according to develop

--- bin/wigner.py
+++ bin/wigner.py
@@ -831,14 +831,15 @@ def determine_normal_modes_format(modes, molecule, nmodes, flag):
                 trace += result[i][i]
                 result[i][i] -= 1
             diagonalcheck[0].append(trace)
-            ## print all matrices
-            #for row in result:
-            #    string = ''
-            #    for entry in row:
-            #        string += "%4.1f" % (float(entry))
-            #    print(string)
-            #print()
-            #############
+            # print all matrices
+            if DEBUG:
+                for row in result:
+                    string = ''
+                    for entry in row:
+                        string += "%4.1f" % (float(entry))
+                    print(string)
+                print()
+            #####################
             if any([abs(i) > thresh for j in result for i in j]):
                 diagonalcheck[1].append(0)
             else:
