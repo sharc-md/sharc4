91524f3ac64e525d009c8c4af6fcd0795339c8c7
smausenberger <sascha.mausenberger@univie.ac.at>
2025-08-19 14:02:17 +0200

    adapt orca test

--- tests/interface/test_orca.py
+++ tests/interface/test_orca.py
@@ -377,7 +377,7 @@ def test_template():
             {
                 "paddingstates": None,
                 "no_tda": False,
-                "basis": "6-31G",
+                "basis": "def2-SVP",
                 "auxbasis": None,
                 "functional": "b3lyp",
                 "dispersion": "D3",
@@ -398,7 +398,7 @@ def test_template():
             {
                 "paddingstates": None,
                 "no_tda": False,
-                "basis": "6-31G",
+                "basis": "def2-SVP",
                 "auxbasis": None,
                 "functional": "b3lyp",
                 "dispersion": "D3",
