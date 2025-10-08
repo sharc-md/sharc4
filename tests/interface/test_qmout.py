6942ba603e9e83426f7b9bbf53a617fc46950178
smausenberger <sascha.mausenberger@univie.ac.at>
2025-10-08 16:50:36 +0200

    add proper qmout parsing tests

--- tests/interface/test_qmout.py
+++ tests/interface/test_qmout.py
@@ -1,8 +1,10 @@
 from qmout import QMout
 from utils import expand_path
 import os
+import pickle
+import numpy as np
 
-PATH = expand_path("$SHARC/../tests/interface/inputs")
+PATH = expand_path("$SHARC/../tests/interface/inputs/qmout")
 
 
 def test_read_qmout():
@@ -21,3 +23,39 @@ def test_read_qmout():
 
     for test in tests:
         QMout(test)
+
+
+def test_parsing():
+    tests = [
+        #(os.path.join(PATH, "421_h_soc_dm_grad_ion"), os.path.join(PATH, "421_h_soc_dm_grad_ion_ref")), # plus notes (turbomole)
+        (os.path.join(PATH, "421_h_soc_dm_grad_ion2"), os.path.join(PATH, "421_h_soc_dm_grad_ion_ref")), # no notes
+        (os.path.join(PATH, "421_h_theodore"), os.path.join(PATH, "421_h_theodore_ref")), # no notes
+        (os.path.join(PATH, "410_h_multipolar"), os.path.join(PATH, "410_h_multipolar_ref")), # plus densities
+        (os.path.join(PATH, "400_h_nacdr"), os.path.join(PATH, "400_h_nacdr_ref")),
+    ]
+
+    for qmout, ref in tests:
+        parsed = QMout(qmout)
+        with open(ref, "rb") as f:
+            reference = pickle.load(f)
+        for k, v in parsed.__dict__.items():
+            if isinstance(v, np.ndarray):
+                assert np.allclose(v, reference[k])
+            elif isinstance(v, list):
+                if k == "prop2d" or k == "prop1d":
+                    for i, j in zip(v, reference[k]):
+                        assert i[0] == j[0], k
+                        if isinstance(i[1], np.ndarray):
+                            assert np.allclose(i[1], j[1]), k
+                        else:
+                            assert i[1] == j[1]
+                else:
+                    assert v == reference[k]
+            elif isinstance(v, dict):
+                for k2, v2 in v.items():
+                    if isinstance(v2, np.ndarray):
+                        assert np.allclose(v2, reference[k][k2])
+                    else:
+                        assert v2 == reference[k][k2]
+            elif v:
+                assert v == reference[k], k
