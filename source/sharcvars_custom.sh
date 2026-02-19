d4166801f83d12b5c5213a8f659940dad23c117e
Lorenz Grünewald <lorenz.gruenewald@univie.ac.at>
2026-02-19 17:15:52 +0100

    removed personal file sharcvars

--- source/sharcvars_custom.sh
+++
@@ -1,4 +0,0 @@
-export SHARC=/user/lorenz/bin/sharc/develop/bin
-export SHARCLIB=/user/lorenz/bin/sharc/develop/lib
-export PYTHONPATH=$SHARCLIB:$PYTHONPATH
-export LD_LIBRARY_PATH=$SHARCLIB:$LD_LIBRARY_PATH
